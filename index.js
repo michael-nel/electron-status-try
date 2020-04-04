7369e1429ca7dd74eef55b52e4e063566afc7f49
Michael Nel <neckware@gmail.com>
2020-04-04 14:35:58 -0300

    Refactory Code!

--- index.js
+++ index.js
@@ -1,44 +1,28 @@
 const path = require('path');
 const electron = require('electron');
+const TimerTray = require('./app/timer_tray');
 
-const { app, BrowserWindow, Tray } = electron;
+const {
+  app,
+  BrowserWindow,
+  Tray
+} = electron;
 
 let mainWindow;
-let tray;
-
-app.on('ready', () =>{
+app.on('ready', () => {
   mainWindow = new BrowserWindow({
-    height:500,
+    height: 500,
     width: 300,
-    frame:false,
+    frame: false,
     resizable: false,
-    show:false,
+    show: false,
     webPreferences: {
-     nodeIntegration: true
-   },
+      nodeIntegration: true
+    },
   });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 
-   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
-   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-   tray = new Tray(iconPath);
-   tray.on('click', (event, bounds) =>{
-
-     const {x , y } = bounds;
-    // Window heigt and width
-     const { height, width } = mainWindow.getBounds();
-
-     if(mainWindow.isVisible()){
-       mainWindow.hide();
-     } else {
-       const yPosition = process.platform === 'darwin' ? y :  y - height;
-       mainWindow.setBounds({
-         x: x - width / 2,
-         y: yPosition,
-         height,
-         width
-       });
-       mainWindow.show();
-      }
-   });
+  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+  new TimerTray(iconPath, mainWindow);
 });
